d2df06fe92f21f397bbe90335e8a901b25235350
Alexandria Curtis <alexandria@Alexandrias-MacBook-Pro-2.local>
2019-07-25 00:26:44 -0500

    Fix profile image sizing

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -5,7 +5,7 @@ class Profile extends Component {
   render() {
     return (
       <div>
-      <div className="profile-image"/>
+      <div className="profile-image"><img height="420px" src="prof.jpg"/></div>
       <div className="name-position-container">
         <div className="name">Alexandria Curtis</div>
         <div className="position">Software Engineer</div>
